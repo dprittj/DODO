b562e033cf565ca6b7aa884a5eee296026576d31
Victoria <veerod0804@yahoo.com>
2022-12-17 17:15:21 -0600

    completed the view for the Login Page

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent} from '../app/login-page/login-page.component'
+import { LoginPageComponent} from '../app/login-page/login-page.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 const routes: Routes = [
