eee78ac3b2eb9a6e8e90d92d313ac1e4334d91cb
Victoria <veerod0804@yahoo.com>
2022-12-30 13:31:21 -0600

    rerouted main localhost to welcome page and routed the login and signup buttons to the designated pages

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { BuildProfileComponent } from './pages/build-profile/build-profile.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
 const routes: Routes = [
   { path: '', component: MainLayoutComponent, children: [
-    { path: '', component: LoginPageComponent }, //add 'login' path after building the welcome page
+    { path: '', component: WelcomePageComponent },
+    { path: 'login', component: LoginPageComponent }, //add 'login' path after building the welcome page
     { path: 'buildnest', component: BuildProfileComponent },
     { path: 'home', component: HomePageComponent },
     {path: 'signup', component: SignUpComponent}
