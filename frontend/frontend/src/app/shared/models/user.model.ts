d6db2a562567daf5c0651c503fe6a133b62c7fcb
Victoria <veerod0804@yahoo.com>
2023-01-03 13:53:38 -0600

    attempting to pull User class, model, and repository

--- frontend/frontend/src/app/shared/models/user.model.ts
+++ frontend/frontend/src/app/shared/models/user.model.ts
@@ -1,7 +1,10 @@
 export class User {
     public id!: number;
+    public email!: string;
     public username!: string;
     public password!: string;
+    public fullName!: string;
+    public location!: string; // possibly switching to int for zipcodes
     
     // constructor(username: string, password: string) {
     //     this.username = username;
