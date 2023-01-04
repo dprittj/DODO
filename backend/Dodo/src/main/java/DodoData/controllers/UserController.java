728afd64ec95b1a5eeb5ca7047abfbe3a7ec077e
Victoria <veerod0804@yahoo.com>
2023-01-04 14:50:25 -0600

    added comments on the UserRespository and UserController. Condensed UserInfoRepository with UserRepository. Waiting on clarification before condensing UserController

--- backend/Dodo/src/main/java/DodoData/controllers/UserController.java
+++ backend/Dodo/src/main/java/DodoData/controllers/UserController.java
@@ -8,20 +8,28 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+//CrossOrigin determines pulling information from the frontEnd.
+// * means it is listening for any data being passed from the frontend
+// maxAge=3600 allows for the session to timeout after 90 minutes of inactivity
+//since it is a RestController, it waits for the frontend to push data back
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
+    //uses JPA userRepository to handle any CRUD operations on the data
     @Autowired
     UserRepository userRepository;
 
+    //registerUser takes in the newUser and saves it in the database
     @PostMapping("/signup")
     public User registerUser(@RequestBody User newUser){
         userRepository.save(newUser);
         return newUser;
     }
 
+    //authenticateUser takes in a user and checks if it exists in the database.
+    //if user exists, we will allow the login.
     @PostMapping("/login")
     public Optional<User> authenticateUser(@RequestBody User user){
         return userRepository.findByUsername(user.getUsername());
